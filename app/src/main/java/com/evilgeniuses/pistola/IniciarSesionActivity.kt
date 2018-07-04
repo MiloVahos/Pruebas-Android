4502d00189332d8cd5f27666bff3ed55c191ad0e
Raykgeneer96 <EvilGeniusesAppsCompany@gmail.com>
2018-07-03 20:22:02 -0500

    avance en login

---
+++ app/src/main/java/com/evilgeniuses/pistola/IniciarSesionActivity.kt
@@ -0,0 +1,32 @@
+/**
+ * @Developer:  Juan Camilo Peña Vahos
+ * @LastUpdate: 02/07/2018
+ *
+ * TODO:    Olvidé la contraseña
+ *
+ */
+
+package com.evilgeniuses.pistola
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class IniciarSesionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+
+        var EEmail          = findViewById(R.id.EEmail) as EditText;
+        var EPassword       = findViewById(R.id.EPassword) as EditText;
+        var BIniciarSesion  = findViewById(R.id.BIniciarSesion) as Button;
+        var BRegistrarse    = findViewById(R.id.BRegistrase)as Button;
+
+        
+
+    }
+
+
+}
--- app/src/main/java/com/evilgeniuses/pistola/MainActivity.kt
+++ app/src/main/java/com/evilgeniuses/pistola/MainActivity.kt
@@ -1,5 +1,15 @@
+/**
+ * @Developer:  Juan Camilo Peña Vahos
+ * @LastUpdate: 02/07/2018
+ *
+ * TODO:    Hacer las políticas de la aplicación
+ * TODO:    Logo de la aplicación
+ * TODO:    Cambiar la fuente de la aplicación?
+ */
+
 package com.evilgeniuses.pistola
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +18,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, IniciarSesionActivity::class.java);
+        startActivity(intent);
+
     }
 }
